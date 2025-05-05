f808e472df60e378cb1a76e53efe5bf924e1afc6
Shra1Yadav <93590332+Shra1Yadav@users.noreply.github.com>
2025-05-05 15:46:57 +0530

    Update terraform-installation.sh

--- terraform-installation.sh
+++ terraform-installation.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+sudo yum install -y yum-utils
 
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 
-gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
-
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-sudo apt update
-
-sudo apt-get -y install terraform
+sudo yum -y install terraform
